d68f7dd4f12eb41e7e848a888a1f72bc12ad06c3
Luciano Kleinhans <72584236+LucianoKleinhans@users.noreply.github.com>
2022-04-25 21:02:40 -0400

    A little help by Mr Philips

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
 
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('source'));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
